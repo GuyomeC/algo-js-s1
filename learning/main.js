b3cc7a3a8d48232affeee0e42194dc28e338acdb
GuyomeC <146094089+GuyomeC@users.noreply.github.com>
2023-09-26 17:36:18 +0100

    adding learning and ex 1 and 2

---
+++ learning/main.js
@@ -0,0 +1,23 @@
+//alert("Tu veux t'en aller ?")
+
+//console.log("bienvenue dans la console")
+
+var cardName = "Choixpeau"
+var age = 18
+var list = []
+var objet = {}
+
+const teacherAge = 28
+const studentAge = 17
+
+if (teacherAge > studentAge) {
+    console.log("the teacher is older than the student")
+} else {
+    console.log("the other thing")
+}
+
+
+
+
+
+
